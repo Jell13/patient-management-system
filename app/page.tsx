f987b2e6b3a520326f61889abb808137822577e3
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-17 13:40:17 -0700

    added the shadcn button

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="text-7xl">
+    <div className="text-7xl text-white underline">
       Home
     </div>
   );
