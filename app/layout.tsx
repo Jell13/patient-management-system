2153ca42e409d42f9cebefc8307f8bd400152ed4
Jason Sugiharto <jasonliesugiharto@gmail.com>
2024-08-17 12:03:12 -0700

    adding the font and font size

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,17 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const fontSans = Plus_Jakarta_Sans({
+  subsets:["latin"],
+  weight:["300", "400", "500", "600", "700"]
+})
+
 export const metadata: Metadata = {
   title: "CarePulse",
-  description: "A patient management system with NextJs, React, Tailwind, Convex, ClerkJs",
+  description: "A patient management system with NextJs, React, Tailwind, AppWrite",
 };
 
 export default function RootLayout({
